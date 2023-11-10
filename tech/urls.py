998a03273397a5112bfd161811b89caf169beca2
Bounama Kounta <bounamakountagoudiaby@gmail.com>
2023-11-10 19:56:04 +0000

    first commit 3

--- tech/urls.py
+++ tech/urls.py
@@ -3,7 +3,7 @@ from tech import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("blogs", views.blogs, name="blogs"),
-
+    path("services", views.service, name="services"),
     path("pricing", views.pricing, name="pricing"),
     path("contact", views.contact, name="contact"),
     path("blogs/blogdetail/<str:title>", views.blogdetail, name="blogdetail"),
